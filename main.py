28fde50ea243a28cfd2846ec1db6163fea48a395
BManju <60623872+BhatiaManju@users.noreply.github.com>
2022-07-18 17:05:24 +0000

    done

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-name= "Manju"
-print('Hello  '+ name)
+# name= "Manju"
+# print('Hello  '+ name)
+# create list
+fruits  =["apple", "orange", "cherry"]
+# print(fruits)
+# #add banana to list fruits
+# fruits.append("banana")
+# print(fruits)
+# print(fruits[0])
+# print(fruits[3])
+# print(fruits[-1])
+# fruits.insert(1, "pineapple")
+# print(fruits)
+# print(len(fruits))
+# for i in range(len(fruits)):
+#   print(fruits[i])
+
+# for value in fruits:
+#   print(value)
+
+# t =(1,2,3,4,5)
+# print(len(t))
